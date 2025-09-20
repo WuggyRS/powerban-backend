8e887e369fe2b6afc8cba41fc18f8f13711bb30c
WuggyRS <52111974+WuggyRS@users.noreply.github.com>
2025-09-20 14:00:55 -0500

    WIP: Attempt to fix today's draw timestamp

--- src/db/knexDb.ts
+++ src/db/knexDb.ts
@@ -1,5 +1,8 @@
 import Knex from "knex";
 import config from "../config/knexConfig.js";
+import pg from 'pg';
+
+pg.types.setTypeParser(pg.types.builtins.DATE, val => val);
 
 const environment = process.env.NODE_ENVIRONMENT || "dev";
 const knex = Knex(config[environment]);
